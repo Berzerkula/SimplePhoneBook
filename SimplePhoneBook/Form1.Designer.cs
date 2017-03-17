239e38efca886214a4307664a124e581e7ba493e
William Harrington <kb0iic@gmail.com>
2017-03-17 14:41:16 -0500

    Add project files.

---
+++ SimplePhoneBook/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿// http://www.instructables.com/id/How-to-create-simple-phone-book-application-C/?ALLSTEPS
+namespace SimplePhoneBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.GRID = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Name1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cityname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Phonenumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.GRID)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // GRID
+            // 
+            this.GRID.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GRID.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name1,
+            this.Name2,
+            this.Cityname,
+            this.Phonenumber});
+            this.GRID.Location = new System.Drawing.Point(0, 27);
+            this.GRID.Name = "GRID";
+            this.GRID.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.GRID.Size = new System.Drawing.Size(779, 310);
+            this.GRID.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(779, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // Name1
+            // 
+            this.Name1.HeaderText = "First Name";
+            this.Name1.Name = "Name1";
+            // 
+            // Name2
+            // 
+            this.Name2.HeaderText = "Last Name";
+            this.Name2.Name = "Name2";
+            // 
+            // Cityname
+            // 
+            this.Cityname.HeaderText = "City";
+            this.Cityname.Name = "Cityname";
+            // 
+            // Phonenumber
+            // 
+            this.Phonenumber.HeaderText = "Phone #";
+            this.Phonenumber.Name = "Phonenumber";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 338);
+            this.Controls.Add(this.GRID);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.GRID)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView GRID;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cityname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Phonenumber;
+    }
+}
+
---
+++ SimplePhoneBook/Form1.cs
@@ -0,0 +1,90 @@
+﻿// http://www.instructables.com/id/How-to-create-simple-phone-book-application-C/?ALLSTEPS
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace SimplePhoneBook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [Serializable] // Allow class to be saved in file
+        public class data // Class for data
+        {
+            public string name;
+            public string surname;
+            public string city;
+            public string number;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GRID.EndEdit();
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog(); // create a file save dialog
+
+            saveFileDialog1.RestoreDirectory = true;
+            //read and filter the raw data
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                FileStream output = new FileStream(saveFileDialog1.FileName,
+                    FileMode.OpenOrCreate, FileAccess.Write);
+
+                int n = GRID.RowCount;
+                data[] Person = new data[n - 1]; // n is number of rows -1 empty row
+                for (int i = 0; i < n - 1; i++)
+                {
+                    Person[i] = new data();
+                    // first number is column index, second is row index. Start from 0
+                    Person[i].name = GRID[0, i].Value.ToString();
+                    Person[i].surname = GRID[1, i].Value.ToString();
+                    Person[i].city = GRID[2, i].Value.ToString();
+                    Person[i].number = GRID[3, i].Value.ToString();
+                }
+
+                formatter.Serialize(output, Person);
+
+                output.Close();
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Read a file and add data to GRID
+            openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                BinaryFormatter reader = new BinaryFormatter();
+                FileStream input = new FileStream(openFileDialog1.FileName, FileMode.Open,
+                    FileAccess.Read);
+                data[] Person = (data[])reader.Deserialize(input);
+                GRID.Rows.Clear();
+                for (int i = 0; i < Person.Length; i++)
+                {
+                    GRID.Rows.Add();
+                    GRID[0, i].Value = Person[i].name;
+                    GRID[1, i].Value = Person[i].surname;
+                    GRID[2, i].Value = Person[i].city;
+                    GRID[3, i].Value = Person[i].number;
+                }
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();  // close app
+        }
+    }
+}
